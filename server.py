0ceb321d2af6e889de87511369082fccff410310
lbh <lbhlbhlbh2002@icloud.com>
2023-08-22 17:12:53 +0800

    server update

--- server.py
+++ server.py
@@ -12,6 +12,10 @@ database: Dict[str, Any] = json.load(gzip.open('database.json.gz'))
 def hello():
     return "<p>Server has successfully set up</p>"
 
+@app.route("/api/teapot")
+def api_teapot():
+    return "<p>I'm a teapot!</p>", 418
+
 @app.route("/api/test")
 def api_test():
     return "success"
@@ -21,7 +25,17 @@ def api_sems(sems: str):
     if sems in database.keys():
         return {'success': True, 'sems': sems}, 200
     else:
-        return {'success': False, 'sems': '0-0-0'}, 404
+        return {'success': False, 'reason': 'invalid sems'}, 404
+    
+@app.route("/api/conflict")
+def api_conflict(method=['GET', 'POST']):
+    if method == 'GET':
+        return {'success': False, 'reason': 'Method not allowed'}, 405
+    elif method == 'POST':
+        pass
+        return {}, 200
+    else:
+        return {'success': False, 'reason': 'Not implemented'}, 501
 
 @app.errorhandler(404)
 def not_found(error):
