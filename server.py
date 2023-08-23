ad89001c68e4a7ce2b6f94faa703412865d77d6a
lbh <lbhlbhlbh2002@icloud.com>
2023-08-23 23:15:09 +0800

    server update

--- server.py
+++ server.py
@@ -1,16 +1,14 @@
 from funcs import data
-from flask import Flask, request
+from flask import Flask, request, redirect, url_for
 from typing import Dict, Any
+from functools import lru_cache
 import json
 import gzip
+import os
 
 app = Flask(__name__)
 
 database: Dict[str, Any] = json.load(gzip.open('database.json.gz'))
-
-@app.route("/")
-def hello():
-    return "<p>Server has successfully set up</p>"
 
 @app.route("/api/teapot")
 def api_teapot():
@@ -20,7 +18,7 @@ def api_teapot():
 def api_test():
     return "success"
 
-@app.route("/api/<sems>")
+@app.route("/api/overview/<sems>")
 def api_sems(sems: str):
     if sems in database.keys():
         return {'success': True, 'sems': sems}, 200
@@ -37,7 +35,24 @@ def api_conflict(method=['GET', 'POST']):
     else:
         return {'success': False, 'reason': 'Not implemented'}, 501
 
+@app.route("/")
+def root_redir():
+    return redirect("/main.html")
+    
+@app.route("/<path:subpath>")
+def static_files(subpath):
+    @lru_cache
+    def getfile(subpath):
+        f = open("visualization/" + subpath, mode='rb')
+        data = f.read()
+        f.close()
+        return data, 200
+    return getfile(subpath)
+
 @app.errorhandler(404)
 def not_found(error):
     return "<p>The resource you requested is not found on the server</p>", 404
 
+@app.errorhandler(FileNotFoundError)
+def not_found_2(error):
+    return "<p>The resource you requested is not found on the server</p>", 404
---
+++ startserver.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/bash
+flask --app server run --debug --host=0.0.0.0 --port=80
