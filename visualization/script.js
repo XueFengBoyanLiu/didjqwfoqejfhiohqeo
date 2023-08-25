a30259338cbca35b3c8ad2ab710a845bdcbce1d1
BoyanLiu <2200011321@stu.pku.edu.cn>
2023-08-25 21:10:54 +0800

    update

--- visualization/script.js
+++ visualization/script.js
@@ -15,7 +15,7 @@ const typeObject = {};
 for (let i = 0; i < typeArray.length; i++) {
     typeObject[typeArray[i]] = i;
 };
-const currentTypes = [];
+const currentTypes = typeArray;
 legends = document.getElementById('legend-of-pie');
 function createCheckBox(key, i) {
     var container = document.createElement('div');
@@ -23,8 +23,10 @@ function createCheckBox(key, i) {
 
     var checkBox = document.createElement('input');
     checkBox.type = 'checkbox';
+    checkBox.checked = true;
     checkBox.id = key;
     checkBox.style.color = colorArray[i];
+    checkBox.style.cursor = 'pointer';
     checkBox.addEventListener('change', () => {
         if (checkBox.checked) {
             currentTypes.push(key);
@@ -32,6 +34,30 @@ function createCheckBox(key, i) {
         else {
             currentTypes.splice(currentTypes.indexOf(key), 1);
         }
+
+        var readyTypes = [];
+        for (let i = 0; i < currentTypes.length; i++) {
+            readyTypes.push(typeObject[currentTypes[i]])
+        };
+        jQuery.ajax({
+            url: "/api/get_typed_courses",
+            type: "post",
+            data: JSON.stringify({ college: currentCollege, qsn: Number(currentSemester.split('-')[0]), xq: Number(currentSemester.split('-')[2]), types: readyTypes }), //:currentTypes.forEach((d)=>{typeObject[d]})
+            dataType: "json",
+            contentType: "application/json",
+            success: (data) => {
+                if (data.success) {
+                    secondGraph(data.data);
+                    // console.log(data.data);
+                }
+                else
+                    window.alert(data.reason);
+            },
+            error: (data) => {
+                console.log(data);
+                window.alert("update failed here");
+            }
+        });
     })
 
     label = document.createElement('label');
@@ -57,7 +83,7 @@ for (let i = 0; i < typeArray.length; i++) {
 }
 
 selectBtn = document.getElementById('one-select-all')
-var selectBtnState = false;
+var selectBtnState = true;
 selectBtn.addEventListener('click', () => {
     if (selectBtnState) {
         for (let i = 0; i < typeArray.length; i++) {
@@ -251,6 +277,7 @@ function initializeSchoolSelector(selectContents) {
 }
 
 function selchgd() {
+
     jQuery.ajax({
         url: "/api/get_trend",
         type: "post",
@@ -383,10 +410,16 @@ function secondGraph(data) {
 
     // Create dummy data
 
+    
     // set the color scale
+
+    var ready_colarArray=[];
+    for(let i=0;i<currentTypes.length;i++){
+        ready_colarArray.push(colorArray[typeObject[Object.keys(data)[i]]]);
+    }
     var color = d3.scaleOrdinal()
         .domain(data)
-        .range(colorArray)
+        .range(ready_colarArray)
 
     // Compute the position of each group on the pie:
     var pie = d3.pie()
@@ -414,13 +447,12 @@ function secondGraph(data) {
     for (let i = 0; i < Object.values(data).length; i++) {
         props.push((Object.values(data)[i] / total * 100).toFixed(2) + '%');
     }
-    console.log('HIHIHIIH');
     var pathes = document.querySelectorAll('#one-course-type svg path');
     for (let i = 0; i < pathes.length; i++) {
         var path = pathes[i];
         path.style.cursor = 'pointer';
         path.addEventListener('mouseover', (event) => {
-            Info = showInfo(Object.keys(data)[i] + ': ' + props[i], event.clientX, event.clientY, colorArray[i], 'white');
+            Info = showInfo(Object.keys(data)[i] + ': ' + props[i], event.clientX, event.clientY, colorArray[typeObject[Object.keys(data)[i]]], 'white');
         })
         path.addEventListener('mouseout', (event) => {
             Info.remove();
@@ -574,8 +606,170 @@ function fourthGraph(heatMapData) {
 }
 
 
-// 初始化
 
+
+// 网络可视化
+const courseSelectorInput = document.getElementById('course-selector-input');
+const courseSelectorButton = document.getElementById('course-selector-button');
+const courseSelector = document.getElementById('course-selector');
+
+courseSelectorButton.addEventListener('click', () => {
+    jQuery.ajax({
+        url: "/api/conflict",
+        type: "post",
+        data: {'kch':courseSelectorInput.value},
+        dataType: "json",
+        contentType: "application/json",
+        success: function (data) {
+            if (data.success)
+                ForceGraph(data.data);
+            else
+                window.alert(data.reason);
+        },
+        error: function (data) {
+            window.alert("查询失败");
+        }
+    });
+})
+
+
+
+function ForceGraph({
+    nodes, // an iterable of node objects (typically [{id}, …])
+    links // an iterable of link objects (typically [{source, target}, …])
+  }, {
+    nodeId = d => d.id, // given d in nodes, returns a unique identifier (string)
+    nodeGroup, // given d in nodes, returns an (ordinal) value for color
+    nodeGroups, // an array of ordinal values representing the node groups
+    nodeTitle, // given d in nodes, a title string
+    nodeFill = "currentColor", // node stroke fill (if not using a group color encoding)
+    nodeStroke = "#fff", // node stroke color
+    nodeStrokeWidth = 1.5, // node stroke width, in pixels
+    nodeStrokeOpacity = 1, // node stroke opacity
+    nodeRadius = 5, // node radius, in pixels
+    nodeStrength,
+    linkSource = ({source}) => source, // given d in links, returns a node identifier string
+    linkTarget = ({target}) => target, // given d in links, returns a node identifier string
+    linkStroke = "#999", // link stroke color
+    linkStrokeOpacity = 0.6, // link stroke opacity
+    linkStrokeWidth = 1.5, // given d in links, returns a stroke width in pixels
+    linkStrokeLinecap = "round", // link stroke linecap
+    linkStrength,
+    colors = d3.schemeTableau10, // an array of color strings, for the node groups
+    width = 640, // outer width, in pixels
+    height = 400, // outer height, in pixels
+    invalidation // when this promise resolves, stop the simulation
+  } = {}) {
+    // Compute values.
+    const N = d3.map(nodes, nodeId).map(intern);
+    const LS = d3.map(links, linkSource).map(intern);
+    const LT = d3.map(links, linkTarget).map(intern);
+    if (nodeTitle === undefined) nodeTitle = (_, i) => N[i];
+    const T = nodeTitle == null ? null : d3.map(nodes, nodeTitle);
+    const G = nodeGroup == null ? null : d3.map(nodes, nodeGroup).map(intern);
+    const W = typeof linkStrokeWidth !== "function" ? null : d3.map(links, linkStrokeWidth);
+    const L = typeof linkStroke !== "function" ? null : d3.map(links, linkStroke);
+  
+    // Replace the input nodes and links with mutable objects for the simulation.
+    nodes = d3.map(nodes, (_, i) => ({id: N[i]}));
+    links = d3.map(links, (_, i) => ({source: LS[i], target: LT[i]}));
+  
+    // Compute default domains.
+    if (G && nodeGroups === undefined) nodeGroups = d3.sort(G);
+  
+    // Construct the scales.
+    const color = nodeGroup == null ? null : d3.scaleOrdinal(nodeGroups, colors);
+  
+    // Construct the forces.
+    const forceNode = d3.forceManyBody();
+    const forceLink = d3.forceLink(links).id(({index: i}) => N[i]);
+    if (nodeStrength !== undefined) forceNode.strength(nodeStrength);
+    if (linkStrength !== undefined) forceLink.strength(linkStrength);
+  
+    const simulation = d3.forceSimulation(nodes)
+        .force("link", forceLink)
+        .force("charge", forceNode)
+        .force("center",  d3.forceCenter())
+        .on("tick", ticked);
+  
+    const svg = d3.create("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .attr("viewBox", [-width / 2, -height / 2, width, height])
+        .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
+  
+    const link = svg.append("g")
+        .attr("stroke", typeof linkStroke !== "function" ? linkStroke : null)
+        .attr("stroke-opacity", linkStrokeOpacity)
+        .attr("stroke-width", typeof linkStrokeWidth !== "function" ? linkStrokeWidth : null)
+        .attr("stroke-linecap", linkStrokeLinecap)
+      .selectAll("line")
+      .data(links)
+      .join("line");
+  
+    const node = svg.append("g")
+        .attr("fill", nodeFill)
+        .attr("stroke", nodeStroke)
+        .attr("stroke-opacity", nodeStrokeOpacity)
+        .attr("stroke-width", nodeStrokeWidth)
+      .selectAll("circle")
+      .data(nodes)
+      .join("circle")
+        .attr("r", nodeRadius)
+        .call(drag(simulation));
+  
+    if (W) link.attr("stroke-width", ({index: i}) => W[i]);
+    if (L) link.attr("stroke", ({index: i}) => L[i]);
+    if (G) node.attr("fill", ({index: i}) => color(G[i]));
+    if (T) node.append("title").text(({index: i}) => T[i]);
+    if (invalidation != null) invalidation.then(() => simulation.stop());
+  
+    function intern(value) {
+      return value !== null && typeof value === "object" ? value.valueOf() : value;
+    }
+  
+    function ticked() {
+      link
+        .attr("x1", d => d.source.x)
+        .attr("y1", d => d.source.y)
+        .attr("x2", d => d.target.x)
+        .attr("y2", d => d.target.y);
+  
+      node
+        .attr("cx", d => d.x)
+        .attr("cy", d => d.y);
+    }
+  
+    function drag(simulation) {    
+      function dragstarted(event) {
+        if (!event.active) simulation.alphaTarget(0.3).restart();
+        event.subject.fx = event.subject.x;
+        event.subject.fy = event.subject.y;
+      }
+      
+      function dragged(event) {
+        event.subject.fx = event.x;
+        event.subject.fy = event.y;
+      }
+      
+      function dragended(event) {
+        if (!event.active) simulation.alphaTarget(0);
+        event.subject.fx = null;
+        event.subject.fy = null;
+      }
+      
+      return d3.drag()
+        .on("start", dragstarted)
+        .on("drag", dragged)
+        .on("end", dragended);
+    }
+  
+    return Object.assign(svg.node(), {scales: {color}});
+  }
+
+
+// 初始化
+selchgd();
 
 
 
